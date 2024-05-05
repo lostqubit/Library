17c07983663c2a21b091f2bc5b946855380cbf0c
lostqubit <sg_suryaprakash@yahoo.com>
2024-05-05 21:33:57 +0530

    generate cards and render UI

---
+++ script.js
@@ -0,0 +1,52 @@
+function Book(title,author,pages,readStatus=false){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+function addBookToLibrary(book){
+    myLibrary.push(book);
+
+    const bookContainer = document.createElement("div");
+    bookContainer.classList.add("card");
+
+    const heading = document.createElement("h3");
+    heading.textContent = book.title;
+
+    const content = document.createElement("div");
+    content.classList.add("content");
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = "Author: " + book.author;
+    const numPages = document.createElement("p");
+    numPages.textContent = "Pages: " + book.pages;
+    content.appendChild(bookAuthor);
+    content.appendChild(numPages);
+
+    const utilityButtons = document.createElement("div");
+    utilityButtons.classList.add("utility");
+    const editButton = document.createElement("img");
+    editButton.src = "/icons/edit.svg"
+    const deleteButton = document.createElement("img");
+    deleteButton.src = "/icons/delete.svg"
+    utilityButtons.appendChild(editButton);
+    utilityButtons.appendChild(deleteButton)
+
+    bookContainer.appendChild(heading);
+    bookContainer.appendChild(content);
+    bookContainer.appendChild(utilityButtons);
+    bookGrid.appendChild(bookContainer); 
+}
+
+const myLibrary = [];
+const bookGrid = document.querySelector(".container");
+
+const book1 = new Book("The Lord of the Rings","J.R.R Tolkien","1000");
+const book2 = new Book("A Song of Ice and Fire","George R.R Martin","800");
+const book3 = new Book("The Alchemist","Paulo Coelhoo","600");
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+console.log(myLibrary);
