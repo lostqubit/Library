f2c903bab40fe40d589e83325987d99d3e4716e6
lostqubit <sg_suryaprakash@yahoo.com>
2024-05-06 11:41:49 +0530

    add book status

--- script.js
+++ script.js
@@ -22,8 +22,28 @@ const addBookToLibrary = (book)=>{
     bookAuthor.textContent = "Author: " + book.author;
     const numPages = document.createElement("p");
     numPages.textContent = "Pages: " + book.pages;
+
+    const statusDiv = document.createElement("div");
+    const statusLabel = document.createElement("label");
+    statusLabel.setAttribute("for",`status-${bookId}`);
+    statusLabel.textContent = "Read: "; 
+    const status = document.createElement("input");
+    status.type = "checkbox";
+    status.id = `status-${bookId}`;
+    status.addEventListener("change",changeReadStatus);
+    if(book.readStatus){
+        status.checked = true;
+        bookContainer.style.backgroundColor = "#6ee7b7"
+    }
+    else{
+        bookContainer.style.backgroundColor = "#fca5a5" ;
+    }
+    statusDiv.appendChild(statusLabel);
+    statusDiv.appendChild(status);
+
     content.appendChild(bookAuthor);
     content.appendChild(numPages);
+    content.appendChild(statusDiv);
 
     const utilityButtons = document.createElement("div");
     utilityButtons.classList.add("utility");
@@ -52,7 +72,14 @@ const deleteBook = (event) =>{
 const createBook = (event) => {
     event.preventDefault();
     const data = new FormData(event.target);
-    const newBook = new Book(data.get("title"),data.get("author"),data.get("pages"));
+    let checkboxStatus;
+    if(data.get("status") ==="finished"){
+        checkboxStatus = true;
+    }
+    else{
+        checkboxStatus = false;
+    }
+    const newBook = new Book(data.get("title"),data.get("author"),data.get("pages"),checkboxStatus);
     addBookToLibrary(newBook);
     addBookForm.reset();
     addOverlay.style.display = "none";
@@ -65,6 +92,7 @@ const editBook = (event) => {
     editTitle.value = myLibrary[selectedId].title;
     editAuthor.value = myLibrary[selectedId].author;
     editPages.value = myLibrary[selectedId].pages;
+    editStatus.checked = myLibrary[selectedId].readStatus;
 
     editId = selectedId;
 }
@@ -76,13 +104,39 @@ const updateBook = (event) => {
     myLibrary[editId].author = data.get("author");
     myLibrary[editId].pages = data.get("pages");
 
+    if(data.get("status")==="finished"){
+        myLibrary[editId].readStatus = true;
+    }
+    else myLibrary[editId].readStatus = false;
+
     document.querySelector(`#card-${editId}>h3`).textContent = myLibrary[editId].title;
-    document.querySelector(`#card-${editId}>div>p:first-child`).textContent = "Author: "+ myLibrary[editId].author;
-    document.querySelector(`#card-${editId}>div>p:last-child`).textContent = "Pages: " + myLibrary[editId].pages;
+    document.querySelector(`#card-${editId} .content>p:first-child`).textContent = "Author: "+ myLibrary[editId].author;
+    document.querySelector(`#card-${editId} .content>p:nth-child(2)`).textContent = "Pages: " + myLibrary[editId].pages;
+    document.querySelector(`#card-${editId} .content>div>input`).checked = myLibrary[editId].readStatus;
+
+    if(myLibrary[editId].readStatus){
+        document.querySelector(`#card-${editId}`).style.backgroundColor = "#6ee7b7";
+    }
+    else{
+        document.querySelector(`#card-${changeId}`).style.backgroundColor = "#fca5a5";
+    }
 
     editId = -1;
     editBookForm.reset();
     editOverlay.style.display = "none";
+}
+
+const changeReadStatus = (event) => {
+    console.log(event.target.id);
+    const changeId = event.target.id.split("-")[1]
+    myLibrary[changeId].readStatus = !myLibrary[changeId].readStatus;
+
+    if(myLibrary[changeId].readStatus){
+        document.querySelector(`#card-${changeId}`).style.backgroundColor = "#6ee7b7";
+    }
+    else{
+        document.querySelector(`#card-${changeId}`).style.backgroundColor = "#fca5a5" ;
+    }
 }
 
 let bookId = 0;
@@ -100,6 +154,7 @@ const editCloseButton = document.querySelector("#edit-close");
 const editTitle = document.querySelector("#edit-title");
 const editAuthor = document.querySelector("#edit-author");
 const editPages = document.querySelector("#edit-pages");
+const editStatus = document.querySelector("#edit-status");
 
 addButton.addEventListener("click", () => {
     addOverlay.style.display = "flex";
@@ -116,9 +171,9 @@ editCloseButton.addEventListener("click", () => {
 })
 editBookForm.addEventListener("submit",updateBook);
 
-const book1 = new Book("The Lord of the Rings","J.R.R Tolkien","1000");
-const book2 = new Book("A Song of Ice and Fire","George R.R Martin","800");
-const book3 = new Book("The Alchemist","Paulo Coelhoo","600");
+const book1 = new Book("The Lord of the Rings","J.R.R Tolkien","1000",true);
+const book2 = new Book("A Song of Ice and Fire","George R.R Martin","800",true);
+const book3 = new Book("The Alchemist","Paulo Coelho","600");
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
